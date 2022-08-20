f496e4d0317b8b8547ba86be17a5eec4f409f1ca
Marwan Taman <marwantaman87@gmail.com>
2022-08-19 21:49:35 -0400

    First Commit Start of Raffle Smart Contract

---
+++ contracts/Raffle.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity ^0.8.7;
+
+error Raffle_NotEnoughEthEntered();
+
+contract Raffle {
+    /* State Variables*/
+    uint256 private immutable i_entranceFee;
+    address payable[] private s_players;
+
+    event RaffleEnter(address indexed player);
+
+    constructor(uint256 entranceFee) {
+        i_entranceFee = entranceFee;
+    }
+
+    function enterRaffle() public payable {
+        if (msg.value < i_entranceFee) {
+            revert Raffle_NotEnoughEthEntered();
+        }
+        s_players.push(payable(msg.sender));
+        emit RaffleEnter(msg.sender);
+    }
+
+    function getEntranceFee() public view returns (uint256) {
+        return i_entranceFee;
+    }
+
+    function getPlayer(uint256 index) public view returns (address) {
+        return s_players[index];
+    }
+}
